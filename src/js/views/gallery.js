4f2ac6edceb0d0f025f425caa1787207136b5d34
Espino <espvtes@gmail.com>
2019-05-07 23:03:12 +0000

    new

--- src/js/views/gallery.js
+++ src/js/views/gallery.js
@@ -4,7 +4,9 @@ import { Galeria2 } from "../component/galeria2.js";
 export class Gallery extends React.Component{
     render(){
         return(
+            <div>
             <Galeria2/>
+            </div>
         );
     }
 }
