9f6121a3e833348ea64ab067259ef0823f7889d2
Espino <espvtes@gmail.com>
2019-05-11 01:03:30 +0000

    add contact

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-3" id="contacto">
+		<div className="row">
+			<div className="col-12 col-sm-3">
+				<ul>
+					<a>Contactos:</a>
+					<br />
+					<span>+5333535333</span>
+					<br />
+					<span>mailto@gmail.com</span>
+				</ul>
+			</div>
+			<div className="col-12 col-sm-3">
+				<ul>
+					<a>Enlaces:</a>
+				</ul>
+			</div>
+			<div className="col-12 col-sm-3">
+				<form />
+			</div>
+		</div>
 	</footer>
 );
