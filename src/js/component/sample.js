df3498de38d94a33805d4189920a1926369be7dc
ESPINO <espvtes@gmail.com>
2019-05-24 21:43:11 +0000

    sticky

--- src/js/component/sample.js
+++ src/js/component/sample.js
@@ -123,8 +123,7 @@ export class Sample extends React.Component {
 									</p>
 									<p>
 										A paragraph is a self-contained unit of a discourse in writing dealing with a
-										particular point or idea. A paragraph consists of one or more sentences. Though
-										not required by the syntax of any language, paragraphs are usually an expected
+										particular p by the syntax of any language, paragraphs are usually an expected
 										part of formal writing, used to organize longer prose.
 									</p>
 									<p>
