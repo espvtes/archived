dd46633bc508e0728599673af8e41a2a8498b626
ESPINO <espvtes@gmail.com>
2019-09-21 05:57:10 +0000

    first commit

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -6,14 +6,20 @@ export class Navbar extends React.Component {
 		return (
 			<nav className="navbar navbar-light bg-light mb-3">
 				<Link to="/">
+					<span className="navbar-brand mb-0 h1">Learn To code</span>
+				</Link>
+				<Link to="/">
+					<span className="navbar-brand mb-0 h1">About as</span>
+				</Link>
+				<Link to="/">
 					<span className="navbar-brand mb-0 h1">
-						React Webapp Boilerplate
+						Open Source Project
 					</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">
-							Check the Context in action
+						<button className="btn btn-outline-primary">
+							Login
 						</button>
 					</Link>
 				</div>
--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 
 import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import "../../styles/index.css";
 
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
+			<div className="container-fluid">
+				<div className="Display-2 text-center">
+					<h1 className="title">
+						<strong>Welcome to BreatheCode.de</strong>
+					</h1>{" "}
+					<h3>
+						<strong>
+							Learn and get better at coding, using technology.
+						</strong>
+					</h3>
+				</div>
+				<div className="container">
+					<div className="row text-center">
+						<div className="col-12 col-sm-6">
+							<div className="button1">
+								<button
+									type="button"
+									className="btn btn-outline-success">
+									Contribute
+								</button>
+							</div>
+						</div>
+						<div className="col-12 col-sm-6">
+							<div className="button1">
+								<button
+									type="button"
+									className="btn btn-outline-primary">
+									Star Learning now
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
